daaeaf54e6b435c961b2cd6462485eb80adfaffd
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-09-07 13:42:55 +0200

    Issue 1: - added the /stockpile command - added the config.yml with configurations for the backend connection - added the stockpileConfig to configure the values of the items as well as regulations. - added the ALApiClient.java to handle backend connections - added a changelog.txt to keep track of all changes - added FactionStockpile and Player Models to represent the backend entitys

--- src/main/java/com/ardaslegends/albaseplugin/AL_Base_Plugin.java
+++ src/main/java/com/ardaslegends/albaseplugin/AL_Base_Plugin.java
@@ -1,17 +1,52 @@
 package com.ardaslegends.albaseplugin;
 
+import com.ardaslegends.albaseplugin.commands.CommandStockpile;
+import com.ardaslegends.albaseplugin.resources.StockpileConfig;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AL_Base_Plugin extends JavaPlugin {
 
+    private static AL_Base_Plugin plugin;
+    private static final String msgPrefix = ChatColor.GOLD + "[AL-Plugin] " + ChatColor.RESET;
+    private static final String errorPrefix = ChatColor.DARK_RED + "[Error]" + ChatColor.RESET;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
 
+        //Setting up the config files
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        StockpileConfig.addDefaults();
+
+        //registering the commands
+        getCommand("stockpile").setExecutor(new CommandStockpile());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    /**
+     * reloading all config files
+     */
+    public void reload() {
+        reloadConfig();
+    }
+
+    public static AL_Base_Plugin getPlugin() {
+        return plugin;
+    }
+
+    public static String getMsgPrefix() {
+        return msgPrefix;
+    }
+
+    public static String getErrorPrefix(){
+        return errorPrefix;
     }
 }
