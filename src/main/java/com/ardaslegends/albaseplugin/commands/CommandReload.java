c3d257fb8be0e5ede5c1710108f472f39ff9777e
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-10-25 09:21:15 +0200

    Issue 10: - Added a CommandReload with Tab completion - Updated CHANGELOG.md

---
+++ src/main/java/com/ardaslegends/albaseplugin/commands/CommandReload.java
@@ -0,0 +1,43 @@
+package com.ardaslegends.albaseplugin.commands;
+
+import com.ardaslegends.albaseplugin.AL_Base_Plugin;
+import com.ardaslegends.albaseplugin.resources.Reloadables;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandReload implements CommandExecutor {
+
+    String msgPrefix = AL_Base_Plugin.getMsgPrefix();
+    String errorPrefix = AL_Base_Plugin.getErrorPrefix();
+
+    /**
+     * The reload command, used to reload either all reloadables or a specific reloadable
+     * @param sender Source of the command
+     * @param command Command which was executed
+     * @param label Alias of the command which was used
+     * @param args Passed command arguments
+     * @return if the syntax is correct, true is returned. On wrong syntax false
+     */
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            AL_Base_Plugin.getPlugin().reload();
+        } else if (args.length == 1) {
+            String feature = args[0].toLowerCase();
+            switch (feature) {
+                case "base":
+                    AL_Base_Plugin.getPlugin().reload(Reloadables.BASE);
+                case "stockpile":
+                    AL_Base_Plugin.getPlugin().reload(Reloadables.STOCKPILE);
+                case "factions":
+                    AL_Base_Plugin.getPlugin().reload(Reloadables.FACTIONS);
+                default:
+                    sender.sendMessage(msgPrefix + errorPrefix + "Nothing to reload with that name.");
+            }
+        } else {
+            sender.sendMessage(msgPrefix + errorPrefix + "Wrong argument count.");
+            return false;
+        }
+        return true;
+    }
+}
